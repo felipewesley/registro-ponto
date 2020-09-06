ecd6a1280b2ad1befc7d2bd2621a24641271414c
Felipe Wesley <fe.wesleybasso@gmail.com>
2020-09-06 03:42:52 -0300

    Layout default creation of application

--- js/controller.js
+++ js/controller.js
@@ -1,1 +1,59 @@
-console.log('hello world')
+class Register {
+    format = {
+        type: undefined, 
+        date: undefined, 
+        hour: undefined
+    }
+    entry
+    lunchInit
+    lunchReturn
+    output
+    constructor(obj){
+        if (obj.entry != undefined) {
+            this.entry = obj.entry
+        }
+        if (obj.lunchInit != undefined) {
+            this.lunchInit = obj.lunchInit
+        }
+        if (obj.lunchReturn != undefined) {
+            this.lunchReturn = obj.lunchReturn
+        }
+        if (obj.output != undefined) {
+            this.output = obj.output
+        }
+    }
+}
+
+class Database {
+        
+    getUser() {
+        return window.localStorage.getItem("user")
+    }
+    setUser(userCode) {
+        window.localStorage.setItem("user", userCode)
+        return 1
+    }
+    getLastInsertion() {
+        return window.localStorage.getItem("lastInsertion")
+    }
+    getTodayRegister() {
+        let data = new Date()
+        let formatedDate = `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`
+        return {
+            key: formatedDate, 
+            content: window.localStorage.getItem(String(formatedDate))
+        }
+    }
+    setRegister(type, hour, date){
+        const r = {
+
+        }
+        new Register(type, hour, date)
+
+        if(this.getTodayRegister().content != null){
+            window.localStorage.setItem(this.getTodayRegister().key, JSON.stringify(r))
+        }
+    }
+}
+
+const db = new Database
